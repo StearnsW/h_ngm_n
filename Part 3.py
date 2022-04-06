c3f3af27abc130f0cbba53a1cdb2c96fee00e40e
Will Stearns <Wstearns@dev-10.com>
2022-04-06 13:41:49 -0500

    Part 3 random word integration

--- Part 3.py
+++ Part 3.py
@@ -1,9 +1,12 @@
-key_word=""
+import random
+
 words_file=open(r"words_alpha(word list from gitHub).txt")
 list_of_words=list(words_file.read().splitlines())
-print(list_of_words[1])
 words_file.close()
-word_knowledge= list("-"*len(key_word))
+
+test_word=random.choice(list_of_words)
+word_knowledge= list("-"*len(test_word))
+
 guesses_remaining=7
 game_in_progress=True
 
@@ -15,7 +18,7 @@ while game_in_progress:
     if valid_guess:
         user_guess=user_guess.lower()
         if len(user_guess)!=1:
-            if user_guess==key_word:
+            if user_guess==test_word:
                 print("You correctly guessed the word!  Congratulations, you won!")
                 game_in_progress=False
             else:
@@ -23,15 +26,15 @@ while game_in_progress:
                 if guesses_remaining>0:
                     print("That is incorrect.")
         else:
-            if key_word.find(user_guess)==-1:
+            if test_word.find(user_guess)==-1:
                 guesses_remaining-=1
                 if guesses_remaining>0:
                     print("That is incorrect.")
             else:
-                for i in range(0,len(key_word)):
-                    if key_word[i]==user_guess:
+                for i in range(0,len(test_word)):
+                    if test_word[i]==user_guess:
                         word_knowledge[i]=user_guess
-                if "".join(word_knowledge)==key_word:
+                if "".join(word_knowledge)==test_word:
                     print("You found all the letters, you won.")
                     game_in_progress=False
     else:
@@ -41,7 +44,7 @@ while game_in_progress:
     if guesses_remaining==0:
         print("That is incorrect, and was your last guess.  I'm sorry, you lost.")
         game_in_progress=False
-print(f'The word was: {key_word}')
+print(f'The word was: {test_word}')
 
 
 
